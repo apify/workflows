f05ba4e024b0d74fca68660c2b57e558ce2ec8bb
Jindřich Bär <jindrichbar@gmail.com>
2024-12-18 14:45:54 +0100

    feat: `no-github` input option for `git-cliff-release` (#165)

--- git-cliff-release/enhance_context.py
+++ git-cliff-release/enhance_context.py
@@ -44,10 +44,16 @@ def enhance_release(
 
 
 def enhance_commit(commit: dict[str, Any], pr_issues: dict[int, list[int]]) -> None:
-    pr_number = commit.get("remote", {}).get("pr_number")
+    commit_remote = commit.get("remote", {})
+
+    pr_number = commit_remote.get("pr_number")
+    username = commit_remote.get("username")
 
     commit["extra"] = commit["extra"] or {}
     commit["extra"]["commit_link"] = f"{repo_url}/commit/{commit['id']}"
+
+    if username:
+        commit["extra"]["username"] = username
 
     if pr_number:
         commit["extra"]["closed_issues"] = pr_issues.get(pr_number, [])
@@ -66,6 +72,7 @@ parser = ArgumentParser()
 parser.add_argument("--repo", type=str, required=True)
 parser.add_argument("--unreleased-version", nargs="?", default=None, type=str)
 parser.add_argument("--release-notes", action=BooleanOptionalAction)
+parser.add_argument("--no-github", default=False, action="store_true")
 
 
 if __name__ == "__main__":
@@ -76,10 +83,11 @@ if __name__ == "__main__":
     pr_issues = load_pr_issues(owner, repo)
     context = json.load(sys.stdin)
 
-    for release in context:
-        enhance_release(release, args.release_notes, args.unreleased_version)
+    if not args.no_github:    
+        for release in context:
+            enhance_release(release, args.release_notes, args.unreleased_version)
 
-        for commit in release["commits"]:
-            enhance_commit(commit, pr_issues)
+            for commit in release["commits"]:
+                enhance_commit(commit, pr_issues)
 
     json.dump(context, sys.stdout)
