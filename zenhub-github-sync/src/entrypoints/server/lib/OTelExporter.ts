e1ca9a0d5f6f4d325e7c4b3fdc2d434d10523aa2
Vlad Frangu <me@vladfrangu.dev>
2025-10-15 11:19:52 +0300

    chore:

--- zenhub-github-sync/src/entrypoints/server/lib/OTelExporter.ts
+++ zenhub-github-sync/src/entrypoints/server/lib/OTelExporter.ts
@@ -1,3 +1,5 @@
+import { deflateSync } from 'node:zlib';
+
 import type { ExportResult } from '@opentelemetry/core';
 import { ConsoleSpanExporter, InMemorySpanExporter, type ReadableSpan } from '@opentelemetry/sdk-trace-node';
 
@@ -9,7 +11,11 @@ export class OTelExporter extends InMemorySpanExporter {
 
 		if (process.env.NODE_ENV !== 'development') {
 			for (const span of spans) {
-				console.log(JSON.stringify(consoleExporter['_exportInfo'](span), null, 2));
+				const exported = consoleExporter['_exportInfo'](span);
+
+				const compressed = deflateSync(JSON.stringify(exported));
+
+				console.log('OTEL', compressed.toString('base64'));
 			}
 		}
 	}
